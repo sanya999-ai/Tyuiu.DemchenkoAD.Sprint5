d708d0cdf07b40b155744ae966fd13dfe949254d
Александра Демченко <Tyuiu.DemchenkoAD@outlook.com>
2024-12-07 19:18:32 +0500

    Task2

---
+++ Tyuiu.DemchenkoAD.Sprint5.Task2.V16.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.DemchenkoAD.Sprint5.Task2.V16.Lib
+{
+    public class DataService : ISprint5Task2V16
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+
+            FileInfo fileInfo = new(path);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists) File.Delete(path);
+
+            string str = "";
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < 0) matrix[i, j] = 0;
+                    else matrix[i, j] = 1;
+
+                    if (j != matrix.GetLength(1) - 1) str += matrix[i, j] + ";";
+                    else str += matrix[i, j];
+                }
+                if (i != matrix.GetLength(0) - 1) File.AppendAllText(path, str + Environment.NewLine);
+                else File.AppendAllText(path, str);
+
+                str = "";
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.DemchenkoAD.Sprint5.Task2.V16/Program.cs
@@ -0,0 +1,80 @@
+﻿using Tyuiu.DemchenkoAD.Sprint5.Task2.V16.Lib;
+namespace Tyuiu.DemchenkoAD.Sprint5.Task2.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #5 | Выполнила: Демченко А. Д. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Демченко Александра |  ИСПб-24-1                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int[,] mass = new int[3, 3];
+
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    Console.Write($"Введите [{i},{j}] элемент массива: ");
+                    mass[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    if (j == mass.GetLength(1) - 1) Console.Write(mass[i, j]);
+                    else Console.Write(mass[i, j] + ", ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = ds.SaveToFileTextData(mass);
+
+            Console.WriteLine("Ответ записан в файл по пути:\n" + path);
+            Console.WriteLine("\nПолученные значения: ");
+
+            string[] massiv = File.ReadAllLines(path).ToArray();
+            int[,] mass2 = new int[massiv.Length, massiv[0].Split(";").Length];
+
+            for (int i = 0; i < massiv.Length; i++)
+            {
+                string[] temp = massiv[i].Split(";");
+                for (int j = 0; j < temp.Length; j++)
+                    mass2[i, j] = Convert.ToInt32(temp[j]);
+            }
+
+            for (int i = 0; i < mass2.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass2.GetLength(1); j++)
+                {
+                    if (j == mass2.GetLength(1) - 1) Console.Write(mass2[i, j]);
+                    else Console.Write(mass2[i, j] + ", ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.ReadLine();
+        }
+    }
+}
